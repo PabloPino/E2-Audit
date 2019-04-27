b6189f078b37631ac610a600304b5cc78348f76e
danielhingar <danielhingar3397@gmail.com>
2019-04-27 17:40:38 +0200

    domain java

---
+++ src/main/java/domain/Sponsorship.java
@@ -0,0 +1,84 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Sponsorship extends DomainEntity {
+
+	// Identification ---------------------------------------------------------
+	// ATRIBUTOS
+	private String	banner;
+	private String	target;
+
+
+	@NotBlank
+	@URL
+	public String getBanner() {
+		return this.banner;
+	}
+
+	public void setBanner(final String banner) {
+		this.banner = banner;
+	}
+
+	@NotBlank
+	@URL
+	public String getTarget() {
+		return this.target;
+	}
+
+	public void setTarget(final String target) {
+		this.target = target;
+	}
+
+
+	// Relationships ---------------------------------------------------------
+
+	private Provider	provider;
+	private CreditCard	creditCard;
+	private Position	position;
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Provider getProvider() {
+		return this.provider;
+	}
+
+	public void setProvider(final Provider provider) {
+		this.provider = provider;
+	}
+
+	@Valid
+	@OneToOne(optional = false)
+	public CreditCard getCreditCard() {
+		return this.creditCard;
+	}
+
+	public void setCreditCard(final CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+	@Valid
+	@ManyToOne(optional = true)
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(final Position position) {
+		this.position = position;
+	}
+
+}
